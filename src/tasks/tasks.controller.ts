1b30632847701b69643342455965d296e40dd582
Gabriela Lima <gabriela.andrade.bueno.lima@gmail.com>
2023-07-21 18:02:58 -0300

    Added delete task by id function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -22,4 +22,10 @@ export class TasksController {
   getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskById(id);
   }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    return this.tasksService.deleteTaskById(id);
+  }
+
 }
