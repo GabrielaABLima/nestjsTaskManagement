537231c26ce546265437d0890984a74bbb581e4c
Gabriela Lima <gabriela.andrade.bueno.lima@gmail.com>
2023-07-23 17:12:46 -0300

    Validation Credentials and Password Strength

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private userEntityRepository: UsersRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
+    return this.userEntityRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository {
@@ -9,4 +10,11 @@ export class UsersRepository {
     @InjectRepository(User)
     private readonly userEntityRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.userEntityRepository.create({ username, password });
+    await this.userEntityRepository.save(user);
+  }
 }
